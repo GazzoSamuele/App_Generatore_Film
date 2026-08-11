bc47c3749d0b2308ed03e6a09e392bc9d0c46b60
Samuele Gazzo <samuelegazzo69@gmail.com>
2026-08-11 17:53:32 +0200

    creazione ed installazione delle dipendenze per il frontend

---
+++ client/src/tipi.ts
@@ -0,0 +1,12 @@
+export interface Raccomandazione {
+  titolo: string;
+  tipo: "film" | "serie";
+  anno: number;
+  generi: string[];
+  piattaforma: string;
+  posterUrl: string;
+  votoMedio: number;
+  compatibilita: number;
+  motivi: string[];
+  punteggio: number;
+}
